c66a9d8ba5fe6c1ac709102d3e90a110a5580011
xsw0 <xsw.mail.0@gmail.com>
2021-03-18 15:03:32 +0800

    14.Chunks of Bytecode

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
-int main()
+int main(int argc, const char *argv[])
 {
-    printf("Hello, World!\n");
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+
     return 0;
 }
