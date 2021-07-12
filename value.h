e07cef4c82266563949664b6d6019ede393c9070
xsw0 <xsw.mail.0@gmail.com>
2021-07-12 13:05:47 +0800

    fix macro

--- value.h
+++ value.h
@@ -34,10 +34,10 @@ typedef struct
 #define AS_NUMBER(value)  ((value).as.number)
 #define AS_OBJ(value)     ((value).as.obj)
 
-#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = (value)}})
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
-#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = (value)}})
+#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)(object)}})
 
 typedef struct
 {
