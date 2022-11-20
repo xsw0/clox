72e1da1a0bb5ced6144d868a0b74059857a2ed4f
xsw0 <xsw.mail.0@gmail.com>
2022-11-20 15:31:11 +0800

    reformat

--- chunk.h
+++ chunk.h
@@ -4,31 +4,29 @@
 #include "common.h"
 #include "value.h"
 
-typedef enum
-{
-    OP_CONSTANT,
-    OP_NIL,
-    OP_TRUE,
-    OP_FALSE,
-    OP_EQUAL,
-    OP_GREATER,
-    OP_LESS,
-    OP_ADD,
-    OP_SUBTRACT,
-    OP_MULTIPLY,
-    OP_DIVIDE,
-    OP_NOT,
-    OP_NEGATE,
-    OP_RETURN,
+typedef enum {
+  OP_CONSTANT,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_NOT,
+  OP_NEGATE,
+  OP_RETURN,
 } OpCode;
 
-typedef struct
-{
-    int count;
-    int capacity;
-    uint8_t *code;
-    int *lines;
-    ValueArray constants;
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t *code;
+  int *lines;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
