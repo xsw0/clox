7e15140f7ee06d30a8c0c5a9a0c6c67f5f269670
xsw0 <xsw.mail.0@gmail.com>
2022-11-20 15:31:11 +0800

    reformat

--- value.h
+++ value.h
@@ -6,44 +6,35 @@
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
 
-typedef enum
-{
-    VAL_BOOL,
-    VAL_NIL,
-    VAL_NUMBER,
-    VAL_OBJ
-} ValueType;
+typedef enum { VAL_BOOL, VAL_NIL, VAL_NUMBER, VAL_OBJ } ValueType;
 
-typedef struct
-{
-    ValueType type;
-    union
-    {
-        bool boolean;
-        double number;
-        Obj *obj;
-    } as;
+typedef struct {
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+    Obj *obj;
+  } as;
 } Value;
 
-#define IS_BOOL(value)    ((value).type == VAL_BOOL)
-#define IS_NIL(value)     ((value).type == VAL_NIL)
-#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
-#define IS_OBJ(value)     ((value).type == VAL_OBJ)
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+#define IS_OBJ(value) ((value).type == VAL_OBJ)
 
-#define AS_BOOL(value)    ((value).as.boolean)
-#define AS_NUMBER(value)  ((value).as.number)
-#define AS_OBJ(value)     ((value).as.obj)
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+#define AS_OBJ(value) ((value).as.obj)
 
-#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = (value)}})
-#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = (value)}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = (value)}})
-#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)(object)}})
+#define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)(object)}})
 
-typedef struct
-{
-    int capacity;
-    int count;
-    Value *values;
+typedef struct {
+  int capacity;
+  int count;
+  Value *values;
 } ValueArray;
 
 bool valuesEqual(Value a, Value b);
--- vm.h
+++ vm.h
@@ -6,20 +6,18 @@
 
 #define STACK_MAX 256
 
-typedef struct
-{
-    Chunk *chunk;
-    uint8_t *ip; // instruction pointer
-    Value stack[STACK_MAX];
-    Value *stackTop;
-    Obj *objects;
+typedef struct {
+  Chunk *chunk;
+  uint8_t *ip; // instruction pointer
+  Value stack[STACK_MAX];
+  Value *stackTop;
+  Obj *objects;
 } VM;
 
-typedef enum
-{
-    INTERPRET_OK,
-    INTERPRET_COMPILE_ERROR,
-    INTERPRET_RUNTIME_ERROR
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 extern VM vm;
